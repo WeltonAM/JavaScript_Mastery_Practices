c20e795e004f3105be92a8019e3481e4be40e2b2
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2025-04-11 11:51:55 -0300

    Fetch movies by search

--- 01_mobile_movie_app/app/(tabs)/search.tsx
+++ 01_mobile_movie_app/app/(tabs)/search.tsx
@@ -1,7 +1,6 @@
 import { ActivityIndicator, FlatList, Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { images } from '@/constants/images'
-import { useRouter } from 'expo-router';
 import useFetch from '@/services/useFetch';
 import { fetchPopularMovies } from '@/services/api';
 import MovieCard from '@/components/MovieCard';
@@ -9,9 +8,29 @@ import { icons } from '@/constants/icons';
 import SearchBar from '@/components/SearchBar';
 
 const Search = () => {
-    const router = useRouter();
+    const [searchQuery, setSearchQuery] = useState('');
 
-    const { data: movies, loading, error } = useFetch(() => fetchPopularMovies({ query: '' }));
+    const { data: movies, loading, error, fetchData, reset } = useFetch(() => fetchPopularMovies({ query: searchQuery }), false);
+
+    const fetchMovies = useCallback(async () => {
+        await fetchData();
+    }, [fetchData]);
+
+    const resetSearch = useCallback(async () => {
+        await reset();
+    }, [reset]);
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            if (searchQuery.trim()) {
+                fetchMovies();
+            } else {
+                resetSearch();
+            }
+        }, 500);
+
+        return () => clearTimeout(timeoutId);
+    }, [searchQuery]);
 
     return (
         <View className='flex-1 bg-primary'>
@@ -36,13 +55,38 @@ const Search = () => {
                         </View>
 
                         <View className='my-5'>
-                            <SearchBar placeholder='Search movies...' />
+                            <SearchBar
+                                placeholder='Search movies...'
+                                value={searchQuery}
+                                onChangeText={(text: string) => setSearchQuery(text)}
+                            />
                         </View>
 
                         {loading && (
-                            <ActivityIndicator size="large" color="#0000ff" />
+                            <ActivityIndicator size="large" color="#0000ff" className='my-3' />
+                        )}
+
+                        {error && (
+                            <Text className='text-red-500 px-5 my-3'>Error: {error?.message}</Text>
+                        )}
+
+                        {!loading && !error && searchQuery.trim() && movies?.length > 0 && (
+                            <Text className='text-xl text-white font-bold'>
+                                Search Results for{' '}
+
+                                <Text className='text-accent'>{searchQuery}</Text>
+                            </Text>
                         )}
                     </>
+                }
+                ListEmptyComponent={
+                    !loading && !error ? (
+                        <View className='mt-10 px-5'>
+                            <Text className='text-center text-zinc-500'>
+                                {searchQuery.trim() ? 'No movies found' : 'Search for a movie'}
+                            </Text>
+                        </View>
+                    ) : null
                 }
             />
         </View>
--- 01_mobile_movie_app/components/SearchBar.tsx
+++ 01_mobile_movie_app/components/SearchBar.tsx
@@ -5,9 +5,11 @@ import { icons } from '@/constants/icons'
 export interface SearchBarProps {
     placeholder: string
     onPress?: () => void
+    value: string
+    onChangeText: (text: string) => void
 }
 
-const SearchBar = ({ placeholder, onPress }: SearchBarProps) => {
+const SearchBar = ({ placeholder, onPress, value, onChangeText }: SearchBarProps) => {
     return (
         <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-2'>
             <Image source={icons.search} className='size-5' resizeMode='contain' tintColor="#ab8bff" />
@@ -15,8 +17,8 @@ const SearchBar = ({ placeholder, onPress }: SearchBarProps) => {
             <TextInput
                 onPress={onPress}
                 placeholder={placeholder}
-                value=""
-                onChangeText={() => { }}
+                value={value}
+                onChangeText={onChangeText}
                 placeholderTextColor="#ffffff50"
                 className='flex-1 ml-2 text-zinc-300'
             />
